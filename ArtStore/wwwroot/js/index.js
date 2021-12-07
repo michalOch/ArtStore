391eb398605acb4a35da8037716145685ae5f4be
Michal Och <ochrobot1990@gmail.com>
2021-12-07 19:37:42 +0100

    add jquary to index.html, and toogle login form by fadeToggle

--- ArtStore/wwwroot/js/index.js
+++ ArtStore/wwwroot/js/index.js
@@ -1,15 +1,29 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
+    console.log("Hello Pluralsight");
 
-var theForm = document.getElementById("the-Form");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToogle = $("#loginToggle");
+    var $popupForm = $(".popupForm");
+
+    $loginToogle.on("click", function () {
+        $popupForm.fadeToggle(500);
+    });
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
+
