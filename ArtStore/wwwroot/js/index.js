480477a5276cb8bad8b2712481eb2826c1279973
Michal Och <ochrobot1990@gmail.com>
2021-12-07 18:09:43 +0100

    add reference to js and  some js example s

---
+++ ArtStore/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello Pluralsight");
+
+var theForm = document.getElementById("the-Form");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
