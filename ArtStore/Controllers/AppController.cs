6328da9ce6654bae2394c2f14f89f14f40516d18
Michal Och <ochrobot1990@gmail.com>
2021-12-08 09:44:12 +0100

    Add controller and main view

---
+++ ArtStore/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ArtStore.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new InvalidProgramException("Bad things happen to good developers");
+            return View();
+        }
+    }
+}
