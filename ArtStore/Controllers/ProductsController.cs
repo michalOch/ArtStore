27781dca51e3c38938350d13024779e5847390e5
Michal Och <ochrobot1990@gmail.com>
2021-12-09 16:32:44 +0100

    add api controller for products and update launchSettings (uncheck run browser)

---
+++ ArtStore/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ArtStore.Data;
+using ArtStore.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace ArtStore.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger )
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Failed to get products: {ex.Message}");  
+            }        
+        }
+    }
+}
