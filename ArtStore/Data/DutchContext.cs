e4e80c7527e8cc42ee542754e0e623ec3361d914
Michal Och <ochrobot1990@gmail.com>
2021-12-09 10:06:18 +0100

    add db context class

---
+++ ArtStore/Data/DutchContext.cs
@@ -0,0 +1,11 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtStore.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
