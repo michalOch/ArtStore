69062e0ee5d3c9a98420b29c64abc8f03003ed8a
Michal Och <ochrobot1990@gmail.com>
2021-12-09 11:30:31 +0100

    use config file in program class and add migration

--- ArtStore/Data/DutchContext.cs
+++ ArtStore/Data/DutchContext.cs
@@ -1,11 +1,25 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ArtStore.Data
 {
     public class DutchContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public DutchContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DutchContextDb"));
+        }
     }
 }
