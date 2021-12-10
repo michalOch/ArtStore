e105857bb1012823eb6fcb985c92f33d0d9187dc
Michal Och <ochrobot1990@gmail.com>
2021-12-10 11:47:29 +0100

    add mapping profile for Order and OrderViewModel

---
+++ ArtStore/Data/DutchMappingsProfile.cs
@@ -0,0 +1,15 @@
+﻿using ArtStore.Data.Entities;
+using ArtStore.ViewModels;
+using AutoMapper;
+
+namespace ArtStore.Data
+{
+    public class DutchMappingsProfile : Profile
+    {
+        public DutchMappingsProfile()
+        {
+            CreateMap<Order, OrderViewModel>()
+                .ForMember( o => o.OrderId, ex => ex.MapFrom(o => o.Id));
+        }
+    }
+}
